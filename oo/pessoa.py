3d12428d0593a5398e15b20c3ef77e5f5f693674
franc <francelmo87@hotmail.com>
2022-10-22 19:07:12 -0300

    Criado atributo de Classe olhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,6 @@
-class Pessoa:        # Classe
+class Pessoa:       # Classe
+    olhos = 2     # ATRIBUTO DE CLASSE ou DEFALT - Caso você tenha um atributo que seja comum a todos os obejtos voce precsia criar
+                    # um valor default antes para economizar memória"""
     def __init__(self, *filhos, nome=None, idade=35):
         self.idade = idade
         self.nome = nome                # self.nome é atributo de dado e nome é parâmetro que ta no parametro de cima
@@ -18,7 +20,14 @@ if __name__ == '__main__':
     print(francelmo.idade)                  # a idade la do método init é passado agora
     for filho in francisco.filhos:          # para cada filho(var) em objeto.parâmetro
         print(filho.nome)                   # imprimir o filho(var).nome
-    francelmo.sobrenome = 'Sousa'           # para se criar atributo em tempo de execução(ou dinâmicamente) passando um novo atributo para o objeto
+    francisco.sobrenome = 'Da silva'           # para se criar atributo em tempo de execução(ou dinâmicamente) passando um novo atributo para o objeto
     del francisco.filhos                    # para deletar os atributos dinamicamento se usa del objeto.atributo
-    print(francisco.__dict__)               # para visualizar todos os atributos de instância de um objeto __dict__
+    francisco.olhos = 1
     print(francelmo.__dict__)               # para visualizar todos os atributos de instância de um objeto __dict__
+    print(francisco.__dict__)               # para visualizar todos os atributos de instância de um objeto __dict__
+    Pessoa.olhos = 3                        # note que estou alterando o atributo da classe Pessoa
+    print(Pessoa.olhos)                     # ele pode se acessado pela classe Pessoa
+    print(francisco.olhos)                  # ele pode se acessado pelo objeto francisco
+    print(francelmo.olhos)                  # ele pode se acessado pelo objeto francelmo
+    print(id(Pessoa.olhos), id(francisco.olhos), id(francelmo.olhos))   #o dander dict primeiro busca as instanacias do
+                                                                        #objeto no dander init depois busca na classe
